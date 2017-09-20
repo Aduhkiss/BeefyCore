52fbdf7af202de5cc16bf52c3d29e7a185d2d39b
Atticus Zambrana <angusbeefsteak14@gmail.com>
2017-09-20 13:14:38 -0700

    Update beef.java

--- src/main/java/net/angusbeefgaming/beefycore/command/beef.java
+++ src/main/java/net/angusbeefgaming/beefycore/command/beef.java
@@ -1,1 +1,16 @@
-
+public class beef implements CommandExecutor {
+  
+  @Override
+  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    String arg1 = args[0];
+    
+    if(sender instanceof Player) {
+      
+      return true;
+    }
+    else {
+     sender.sendMessage("You must run this command in game!");
+      return false;
+    }
+  }
+}
